65bb56c173637a8f377545e6798faad43b511717
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2023-02-02 17:48:43 +0300

    feat(client): add light and dark themes

---
+++ PWAConverter/Client/src/app/models/theme.ts
@@ -0,0 +1,4 @@
+export enum Theme {
+  Light = 'light-theme.css',
+  Dark = 'dark-theme.css'
+}
---
+++ PWAConverter/Client/src/app/services/theme.service.ts
@@ -0,0 +1,38 @@
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Theme } from '../models/theme';
+import { BehaviorSubject } from 'rxjs';
+
+const THEME_KEY = 'active-theme';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  activeTheme$: BehaviorSubject<Theme>;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.activeTheme$ = new BehaviorSubject(this.getThemeOrDefault());
+    this.activeTheme$.subscribe((theme) => this.switchTheme(theme));
+  }
+
+  switchTheme(theme: Theme) {
+    const themeLink = this.document.getElementById(
+      'app-theme'
+    ) as HTMLLinkElement;
+    themeLink.href = theme;
+  }
+
+  getThemeOrDefault(): Theme {
+    let theme = localStorage.getItem(THEME_KEY);
+    if (!theme) {
+      theme = Theme.Light;
+    }
+    return theme as Theme;
+  }
+
+  setTheme(theme: Theme) {
+    this.activeTheme$.next(theme);
+    localStorage.setItem(THEME_KEY, theme);
+  }
+}
