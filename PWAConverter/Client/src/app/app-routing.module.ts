29754b0d90a9f33025e32b3d1b58a8993128f43e
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2023-02-04 14:48:03 +0300

    feat(client): add routes

--- PWAConverter/Client/src/app/app-routing.module.ts
+++ PWAConverter/Client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
