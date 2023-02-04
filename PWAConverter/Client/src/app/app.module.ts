b4434e2519f39838b620fbb380c98a2351f0e6bf
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2023-02-04 14:50:22 +0300

    feat(client): add sidebar

--- PWAConverter/Client/src/app/app.module.ts
+++ PWAConverter/Client/src/app/app.module.ts
@@ -5,15 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {ButtonModule} from 'primeng/button';
+import {CheckboxModule} from 'primeng/checkbox';
+import {InputTextModule} from 'primeng/inputtext';
+
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
