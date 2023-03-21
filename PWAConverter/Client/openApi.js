d1a8e8e8888f23a1ef3ea17a8b809ebd26c13936
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2023-03-21 20:12:18 +0300

    build(openapi.js): update url

--- PWAConverter/Client/openApi.js
+++ PWAConverter/Client/openApi.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 https
-  .get(`https://localhost:7194/swagger/v1/swagger.yaml`, resp => {
+  .get(`https://localhost:7194/v1/openapi/pwa-converter.yaml`, resp => {
     let data = "";
 
     // A chunk of data has been recieved.
@@ -13,16 +13,17 @@ https
 
     // The whole response has been received. Print out the result.
     resp.on("end", () => {
-      fs.writeFile("./openApi.yaml", data, function(err) {
-        if(err) {
-            return console.log(err);
+      fs.writeFile("./openApi.yaml", data, function (err) {
+        if (err) {
+          return console.log(err);
         }
         console.log("The file was saved!");
+      });
     });
-        });
   })
   .on("error", err => {
     console.log("Error: " + err.message);
+    process.exit(1);
   });
 
 
