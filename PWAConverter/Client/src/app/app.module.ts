7a69ae9725c161254534cdb62b562b0d51443ad5
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2023-03-23 16:39:57 +0300

    feat(signup): add validators

--- PWAConverter/Client/src/app/app.module.ts
+++ PWAConverter/Client/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import {ButtonModule} from 'primeng/button';
-import {CheckboxModule} from 'primeng/checkbox';
-import {InputTextModule} from 'primeng/inputtext';
-
-
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { BlockUIModule } from 'primeng/blockui';
+import { FormsModule } from '@angular/forms';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
 
 @NgModule({
   declarations: [
@@ -24,16 +26,21 @@ import { ProfileComponent } from './components/profile/profile.component';
     SignupComponent,
     HomeComponent,
     ProjectsComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     ButtonModule,
     CheckboxModule,
-    InputTextModule
+    InputTextModule,
+    PasswordModule,
+    DividerModule,
+    BlockUIModule,
+    AvatarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- PWAConverter/Client/src/app/components/signup/signup.component.ts
+++ PWAConverter/Client/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RegisterRequest } from 'src/OpenApiClient';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  user = new RegisterRequest ({name: "", email: "", password: ""});
 
+  signup(){
+    console.log(this.user);
+  }
 }
