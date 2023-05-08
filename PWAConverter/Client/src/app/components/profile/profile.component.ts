d87d416a438ca7bc59608c9dbf587bcf0cd2c880
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2023-05-08 17:42:15 +0300

    feat(profile): add profile template

--- PWAConverter/Client/src/app/components/profile/profile.component.ts
+++ PWAConverter/Client/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UpdateEmailRequest } from 'src/OpenApiClient';
+import { UpdatePasswordRequest, UserClient } from 'src/OpenApiClient';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  passwordDialogVisible = false;
+  emailDialogVisible = false;
+  changePasswordModel = new UpdatePasswordRequest({password: "", newPassword: ""});
+  changeEmailModel = new UpdateEmailRequest({password: "", newEmail:""});
+
+  constructor(private userClient:UserClient){}
+
+  user$ = this.userClient.getUser();
+
+  showPasswordDialog(){
+    this.passwordDialogVisible = true;
+  }
+
+  showEmailDialog(){
+    this.emailDialogVisible = true;
+  }
+
+  deleteUserAccount(){
+    this.userClient.deleteUser();
+  }
 
 }
